5a1fc62de50e4bf8b94a28718d5239248a133293
Michal Bujalski <bujalski.michal@gmail.com>
2020-05-10 20:03:12 +0200

    First commit

---
+++ src/pages/UserSearch.page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+
+const SearchPage = styled.button`
+  paddin: 4em;
+  background-color: red;
+`;
+
+export default () => {
+  return (
+    <SearchPage>
+      <h1>hello</h1>
+    </SearchPage>
+  );
+};
